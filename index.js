41423f766f3c9fb3c273f1535e664b45439f2afe
alexander-8989 <alexander.m@appwrk.com>
2022-05-12 19:32:58 +0530

    Customer service page is done

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ function changeBg() {
     }
 }
 window.addEventListener('scroll', changeBg);
+
+
+// Validation for FAQ Tab Change
+
+function myFaq(evt, idName) {
+    var i, faq_section, faq_butt;
+    faq_section = document.getElementsByClassName("faq_section");
+    for (i = 0; i < faq_section.length; i++) {
+        faq_section[i].style.display = "none";
+    }
+    faq_butt = document.getElementsByClassName("faq_butt");
+    for (i = 0; i < faq_butt.length; i++) {
+        faq_butt[i].className = faq_butt[i].className.replace(" active", "");
+    }
+    document.getElementById(idName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+
